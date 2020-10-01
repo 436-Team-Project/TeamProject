8fce4a467851bed3185e128650b54e9e1a9f3c53
jayaguirre <jayaguirre@email.arizona.edu>
2020-09-30 18:15:24 -0700

    Copied Ben's files into the src folder.

---
+++ src/Objects/Spots.java
@@ -0,0 +1,19 @@
+package Objects;
+
+
+public class Spots extends UIObjects{
+	
+	boolean available= true;
+	
+	public Spots(int ID, int x, int y, int x2, int y2) {
+		super(ID, x, y, x2, y2);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public void draw() {
+		//draw a circle or chair in this spot.
+		
+	}
+	
+}
