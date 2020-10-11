a678a7cddc8863b21ea9dcd1e609dd2c514ae419
Ben Taylor <bentaylor012@email.arizona.edu>
2020-10-11 14:08:40 -0700

    added createnewobject and updatenewobject to the controller as well as a shell version for the model

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,6 +1,7 @@
 package Controller;
 
 import Model.Model;
+import View.View;
 
 /**
  * Get commands from the View and controls the Model
@@ -8,9 +9,23 @@ import Model.Model;
 public class Controller {
 	
 	private Model model;
+	private View view;
 	
 	public Controller() {
 		model = new Model();
+		view = new View();
+	}
+	/*
+	 * param: wall, seat, or table string. this adds to the object to the model
+	 */
+	public void createNewObject(String type) {
+		model.createObject(type);
+	}
+	/*
+	 * param: the x and y coords and the ID.
+	 */
+	public void updateCurrentObject(int x, int y, int ID) {
+		model.updateObject(x, y, ID);
 	}
 }
 
--- src/Model/Model.java
+++ src/Model/Model.java
@@ -1,11 +1,13 @@
 package Model;
 
+import java.util.ArrayList;
 import java.util.Observable;
 
 /**
  * The state of the application
  */
 public class Model extends Observable {
+	ArrayList<UIObjects> itemList = new ArrayList<UIObjects>();
 	
 	/**
 	 * Constructor
@@ -18,9 +20,14 @@ public class Model extends Observable {
 	/**
 	 * Template method (ignore)
 	 */
-	public void doSomething(){
+	public void createObject(String type){
 		setChanged();
 		notifyObservers();
+		System.out.format("new object added. %d items exist", itemList.size());
+	}
+	
+	public void updateObject(int x, int y, int ID) {
+		System.out.println("updated");
 	}
 	
 }
