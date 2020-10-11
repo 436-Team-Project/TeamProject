b9fe30865dfc15a2b3dd1e21f845d97bc868e366
Ben Taylor <bentaylor012@email.arizona.edu>
2020-10-11 15:28:03 -0700

    added more functionality.

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,6 +1,7 @@
 package Controller;
 
 import Model.Model;
+import Model.UIObjects;
 import View.View;
 
 /**
@@ -16,16 +17,20 @@ public class Controller {
 		view = new View();
 	}
 	/*
-	 * param: wall, seat, or table string. this adds to the object to the model
+	 * param: "wall, seat, or table" string. this adds to the object to the model
 	 */
-	public void createNewObject(String type) {
-		model.createObject(type);
+	public void createNewObject(String type, int x, int y) {
+		model.createObject(type, x, y);
 	}
 	/*
 	 * param: the x and y coords and the ID.
 	 */
 	public void updateCurrentObject(int x, int y, int ID) {
 		model.updateObject(x, y, ID);
+	}
+	
+	public UIObjects getObject(int x, int y) {
+		return model.getObject(x,y);
 	}
 }
 
