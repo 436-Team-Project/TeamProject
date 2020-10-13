2c2ab15e958b5f395c08e1e39fe46a25ac865b20
Ben Taylor <bentaylor012@email.arizona.edu>
2020-10-12 19:01:35 -0700

    updated the update functions

--- src/Model/Tables.java
+++ src/Model/Tables.java
@@ -11,16 +11,7 @@ public class Tables extends UIObjects {
 	int numberOfSpots;
 	ArrayList<Spots> availableSpots = new ArrayList<Spots>();
 	
-	public Tables(int ID, double x, double y, double x2, double y2) {
-	/**
-	 * Table constructor
-	 *
-	 * @param ID table ID
-	 * @param x vertical position
-	 * @param y horizontal position
-	 * @param x2 vertical position
-	 * @param y2 horizontal position
-	 */
+	
 	public Tables(int ID, double x, double y, double x2, double y2) {
 		super(ID, x, y, x2, y2);
 		this.numberOfSpots = 4;
